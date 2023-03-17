d6df13a1cdf34870402666e7213541d932433f71
Hambarakou <djiteye54@gmail.com>
2023-03-17 16:08:37 -0700

    positive or negative

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,15 +15,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 5)
 {
-printf("The last digit of %d is %d%10 and is greater than 5", n);
+printf("The last digit of %d is %d%10 and is greater than 5", n, n);
 }
 else if (n == 0)
 {
-printf("The last digit of %d is %d%10 and is 0", n);
+printf("The last digit of %d is %d%10 and is 0", n, n);
 }
 else(n < 6 && != 0)
 {
-printf("The last digit of %d is %d%10 and is less than 6 and not 0", n);
+printf("The last digit of %d is %d%10 and is less than 6 and not 0", n, n);
 }
 return (0);
 }
