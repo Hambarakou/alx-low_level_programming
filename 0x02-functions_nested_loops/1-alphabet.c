ef455ba0a866a6663d8608626075b0fa320e9277
Hambarakou <djiteye54@gmail.com>
2023-03-21 04:55:10 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+int main(void)
+{
+void print_alphabet(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+putchar(ch/n);
+}
+return (0);
+}
+return (0);
+}
