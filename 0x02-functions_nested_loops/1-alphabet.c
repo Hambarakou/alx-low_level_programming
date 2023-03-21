d54fd4f13c606b0f6b9ea0f66213cf679267d7cb
Hambarakou <djiteye54@gmail.com>
2023-03-21 11:31:52 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,14 +5,17 @@
 *
 * Return: Always 0 (Success)
 */
+void print_alphabet(void);
 int main(void)
 {
+return (0);
+}
 /**
 * print_alphabet - alphabet
 *
 * Return: void
 */
-void print_alphabet();
+void print_alphabet(void)
 {
 char ch;
 for (ch = 'a' ; ch <= 'z' ; ch++)
@@ -21,6 +24,4 @@ putchar(ch);
 }
 return (void);
 }
-print_alphabet();
-return (0);
-}
+
